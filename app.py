9ec69c8c10bfd1f00118e17e294e92ae89cf7e7f
jrhacker1010-coder <jrhacker1010@gmail.com>
2025-12-20 23:35:24 +0530

    app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ elif menu == "Bulk Resume Screening":
                 })
 
             
-                 df = pd.DataFrame(results).sort_values(
+               df = pd.DataFrame(results).sort_values(
                  by="Match Score (%)", ascending=False
                 )
 
@@ -230,6 +230,7 @@ elif menu == "Final Decision & Email":
         st.success("📧 Result emails sent to candidates (simulated)")
     else:
         st.warning("Please complete resume screening first.")
+
 
 
 
